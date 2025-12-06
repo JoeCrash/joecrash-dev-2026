d32435d48503bc9a57d8285c7c604a68b71c63ed
JoeCrash <joeycrash135@gmail.com>
2025-12-06 03:46:48 -0500

    first commit. project started

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Navbar from "#components/Navbar.jsx";
+
+const App = () => {
+    return (
+        <main>
+            <Navbar />
+            <h1 className="text-3xl font-bold underline">Welcome to my portfolio</h1>
+        </main>
+
+    )
+}
+
+export default App
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,31 @@
+import dayjs from "dayjs";
+
+import {navIcons, navLinks} from "../constants";
+
+
+const Navbar = () => {
+    return (
+        <nav>
+            <div>
+                <img src="/images/logo.svg" alt="logo"/>
+                <p className="font-bold">JoeCrash's Portfolio</p>
+                <ul>
+                    {navLinks.map(({id, name}) =>
+                        <li key={id}><p>{name}</p></li>)
+                    }
+                </ul>
+            </div>
+            <div>
+                <ul>
+                    {navIcons.map(({id, img}) =>
+                        <img src={img} alt={`icon-${id}`}></img>)
+                    }
+                </ul>
+                <time>
+                    {dayjs().format("ddd MMM D h:mm A")}
+                </time>
+            </div>
+        </nav>
+    )
+}
+export default Navbar
