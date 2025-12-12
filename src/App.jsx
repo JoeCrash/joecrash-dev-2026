bf008712b58de6b150d5b04b7c2621531a13a557
JoeCrash <joeycrash135@gmail.com>
2025-12-12 00:08:37 -0500

    complete changes to window headers

--- src/App.jsx
+++ src/App.jsx
@@ -14,7 +14,7 @@ const App = () => {
             <TextViewer />
             <ImageViewer />
             <Contact />
-            {/* <Gallery /> */}
+            <Gallery />
             <Home />
         </main>
 
