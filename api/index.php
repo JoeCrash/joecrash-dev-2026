d9caaa2c100784a079dde91173f402bcf6a69e72
JoeCrash <joeycrash135@gmail.com>
2025-12-13 18:41:54 -0500

    apply CR changes, add guard to php endpoint while in dev

--- api/index.php
+++ api/index.php
@@ -1,3 +1,11 @@
 <?php
+// IP whitelist for development only
+$allowed_ips = ['127.0.0.1', '::1'];
+if (!in_array($_SERVER['REMOTE_ADDR'], $allowed_ips)) {
+    http_response_code(403);
+    die('Access denied');
+}
+
+// Add additional authentication here
 phpinfo();
 ?>
