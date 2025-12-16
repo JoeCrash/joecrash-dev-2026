b1cf203ee760b0a744da1b08a7a42697cec9995f
JoeCrash <joeycrash135@gmail.com>
2025-12-16 13:54:10 -0500

    add close tooltip to window controls

--- src/components/WindowControls.jsx
+++ src/components/WindowControls.jsx
@@ -1,4 +1,6 @@
 import useWindowStore from "#store/window.js";
+import {Tooltip} from "react-tooltip";
+import React from "react";
 
 const WindowControls = ({ target }) => {
     const { closeWindow } = useWindowStore();
@@ -6,7 +8,13 @@ const WindowControls = ({ target }) => {
         <div id="window-controls">
             <div className="maximize" />
             <div className="minimize" />
-            <div className="close" onClick={() => closeWindow(target)} />
+            <div className="close"
+                 onClick={() => closeWindow(target)}
+                 data-tooltip-id="close-window-tooltip"
+                 data-tooltip-content="Close Window"
+                 data-tooltip-delay-show={150}
+            />
+            <Tooltip id="close-window-tooltip" place="right" className="tooltip" />
         </div>
     )
 }
